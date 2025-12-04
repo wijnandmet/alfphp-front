d70938baebc1b677a8abb99dfc58eff85a8bfb1d
Wijnand de Ridder <wderidder@landstede.nl>
2025-12-04 16:20:27 +0100

    mooier maken

--- public/index.php
+++ public/index.php
@@ -2,26 +2,16 @@
 use ALF\View;
 use ALF\Route;
 
-
 try {
     require_once './vendor/autoload.php';
-
-    $env = parse_ini_file('.env');
-    env(null, $env);
-
+    env(null, parse_ini_file('.env'));
     include_once './App/routes.php';
 
     echo Route::load();
 } catch (Exception $exception) {
     http_response_code(404);
-    $view = View::load('errors/404.php');
-
-    if ($view->exist('errors/404d.php')) {
-        echo $view->with(['error' => exception_to_error($exception)])->render();
-    } else {
-        echo $view->with(['error' => exception_to_error($exception)])->renderFromPackage();
-    }
+    echo View::load('errors/404.php')->with(['error' => exception_to_error($exception)])->render();
     exit;
 }
 
-register_shutdown_function(shutdown());
+register_shutdown_function('shutdown');
